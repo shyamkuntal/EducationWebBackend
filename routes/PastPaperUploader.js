753def537aba1b5756a2b789e69bce4eb91b6383
shivam malviya <shivam@turnkey.work>
2023-05-19 10:14:20 +0530

    PastPaper

--- routes/PastPaperUploader.js
+++ routes/PastPaperUploader.js
@@ -1,10 +1,17 @@
-import express from "express";
-import { createPastPaper } from "../controller/PastPaperUploader/PastPaper.js";
-import upload from "../config/multer.js";
+const express = require("express");
+const upload = require("../config/multer.js");
+const {
+  createPastPaper,
+  getAssignedSheets,
+  SubmitToSupervisor,
+  Markitascomplete,
+  getallassignedsheetsubjects,
+} = require("../controller/PastPaperUploader/PastPaper.js");
 
 const router = express.Router();
 
-//router.get("/:userId/getassignedsheets", getAssignedSheets);
+router.get("/:userId/getassignedsheets", getAssignedSheets);
+router.get("/:userId/getassignedsubjects", getallassignedsheetsubjects);
 router.post(
   "/createpastpaper",
   upload.fields([
@@ -14,5 +21,7 @@ router.post(
   createPastPaper
 );
 // router.patch("/:sheetId/changestatus", ChangeStatus);
-// router.patch("/:sheetId/submittosupervisor", SubmitToSuperVisor);
-export default router;
+router.patch("/:sheetId/submittosupervisor", SubmitToSupervisor);
+router.patch("/:sheetId/markitascomplete", Markitascomplete);
+router.put("/:ppId/editpastpaper", EditPastPaper);
+module.exports = router;
