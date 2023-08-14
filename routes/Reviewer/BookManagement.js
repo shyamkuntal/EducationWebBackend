7c92adca9b27be0c43d7d696a2028a1645ae00af
shyam kuntal <chaudharyshyam1355@gmail.com>
2023-08-14 14:12:29 +0530

    Book Management Reviewer Service

---
+++ routes/Reviewer/BookManagement.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const paginatedBookTasks = require("../../middlewares/paginatedBookTask");
+
+const router = express.Router();
+
+router.get("/getallpaginatedbooktask", paginatedBookTasks(), (req, res) => {
+  res.json(res.paginatedResults);
+});
