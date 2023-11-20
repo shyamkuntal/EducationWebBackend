e1cd162fa3eff4d28069d00845e9bdf178b70ab0
Ayush Naik <ayush@turnkey.work>
2023-11-20 15:44:40 +0530

    logger memory leak fixed

--- utils/logger.js
+++ utils/logger.js
@@ -1,25 +1,30 @@
-const { createLogger, format, transports } = require('winston');
-const DailyRotateFile = require('winston-daily-rotate-file');
+const { createLogger, format, transports } = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
 
 const logFormat = format.printf(
-    ({ timestamp, label, level, message }) => `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`,
+  ({ timestamp, label, level, message }) =>
+    `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`
 );
 
-const info = new DailyRotateFile({
-    filename: 'logs/%DATE%.log',
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxSize: '30m',
-    maxFiles: '14d',
-    level: 'info',
-    handleExceptions: true,
+const transport = new DailyRotateFile({
+  filename: "logs/%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  zippedArchive: true,
+  maxSize: "30m",
+  maxFiles: "14d",
+  level: "info",
+  prepend: true,
 });
 
-let transport = [new transports.Console({ silent: false }), info];
+// let transport = [new transports.Console({ silent: false }), info];
 
 const logger = createLogger({
-    format: format.combine(format.timestamp(), format.label({ label: process.env.APP_LABEL || 'EDUPLANET' }), logFormat),
-    transports: transport,
+  format: format.combine(
+    format.timestamp(),
+    format.label({ label: process.env.APP_LABEL || "EDUPLANET" }),
+    logFormat
+  ),
+  transports: [transport, new transports.Console({ level: "info" })],
 });
 
 module.exports = logger;
