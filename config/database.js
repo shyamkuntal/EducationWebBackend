bcfe3d0ee91638d9744bc91764f916f6093a7cdc
Ayush Naik <ayush@turnkey.work>
2023-11-20 15:51:59 +0530

    inccreased max pool size

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ const db = new Sequelize(
     dialect: "postgres",
     synchronize: true, // Automatically creates tables based on model definitions
     pool: {
-      max: 5,
+      max: 10,
       min: 0,
       acquire: 30000,
       idle: 10000,
